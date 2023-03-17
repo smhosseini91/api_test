21e4ce73d5a6a45a9f1dc99b2282c6a2adcdf6e0
Syed <test@test.com>
2023-03-17 04:20:08 +0400

    feat/add /sum endpoint

--- sets/views.py
+++ sets/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
+from .models import Set
 
-# Create your views here.
+
+class Sum(View):
+    def get(self, request):
+
+        try:
+            num_a = int(request.GET.get('a'))
+            num_b = int(request.GET.get('b'))
+        except ValueError:
+            raise ValueError("a and b should be provided as Integer values.")
+        except TypeError:
+            raise ValueError("a and b numbers should be provided.")
+
+        Set.objects.create(num_a=num_a, num_b=num_b)
+
+        return JsonResponse({'result': num_a + num_b})
