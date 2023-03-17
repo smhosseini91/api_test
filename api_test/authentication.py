e0d7277d770f5a6999500e0812f33722ac5db4ff
Syed <test@test.com>
2023-03-17 22:23:37 +0400

    Feat: add basic header authentication

--- api_test/authentication.py
+++ api_test/authentication.py
@@ -1,18 +1,35 @@
+import base64
 import functools
 from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.middleware import RemoteUserMiddleware
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 
 
 class AuthenticationBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
+        if 'HTTP_AUTHORIZATION' in request.META:
+            auth = request.META['HTTP_AUTHORIZATION'].split()
+            if len(auth) == 2:
+                if auth[0].lower() == "basic":
+                    username, password = base64.b64decode(auth[1]).decode("utf8").split(':', 1)
+
         if username == 'admin' and password == 'admin':
             admin_user = User.objects.filter(username='admin').first()
             if not admin_user:
                 admin_user = User.objects.create_superuser('admin', 'admin@admin.com', 'admin')
             return admin_user
         else:
-            return super().authenticate(request.username, password, **kwargs)
+            user = User.objects.filter(username=username).first()
+            if not user:
+                return
+            if not user.check_password(password):
+                return
+            return user
+
+
+class HeaderAuthenticationMiddleware(RemoteUserMiddleware):
+    header = 'HTTP_AUTHORIZATION'
 
 
 def ajax_login_required(view_func):
--- api_test/settings.py
+++ api_test/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-fddnd@u$9_3%f6nd6@5r870#y1kg!c-yut$es&u64)y_2j))!$
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -46,8 +44,14 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'api_test.authentication.HeaderAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
+AUTHENTICATION_BACKENDS = [
+    'api_test.authentication.AuthenticationBackend',
 ]
 
 ROOT_URLCONF = 'api_test.urls'
@@ -71,7 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'api_test.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -81,7 +84,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -101,10 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTHENTICATION_BACKENDS = [
-    'api_test.authentication.AuthenticationBackend'
-]
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -115,7 +113,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
